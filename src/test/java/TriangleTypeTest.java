84f2ed0b2c0bc3e79ce1eed50bb07c5ea02cb56e
Alvin Huynh <avh670@gmail.com>
2019-11-10 17:01:17 -0600

    Created basic framework for test cases

---
+++ src/test/java/TriangleTypeTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TriangleTypeTest {
+    @Test
+    public void TypeTest1() {
+        
+    }
+
+    @Test
+    public void TypeTest2() {
+
+    }
+
+    @Test
+    public void TypeTest3() {
+
+    }
+
+    @Test
+    public void TypeTest4() {
+
+    }
+
+    @Test
+    public void TypeTest5() {
+
+    }
+}
---
+++ src/test/java/TriangleValidityTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Test;
+
+public class TriangleValidityTest {
+    @Test
+    public void ValidityTest1() {
+
+    }
+
+    @Test
+    public void ValidityTest2() {
+
+    }
+
+    @Test
+    public void ValidityTest3() {
+
+    }
+
+    @Test
+    public void ValidityTest4() {
+
+    }
+
+    @Test
+    public void ValidityTest5() {
+
+    }
+}
